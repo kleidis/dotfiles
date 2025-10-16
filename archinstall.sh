5ba614d78ac4a6a1895476a564cf8dc80f60e4da
Inix <Nixy01@proton.me>
2025-10-16 17:26:53 +0200

    artix

--- archinstall.sh
+++ archinstall.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Define packages to be installed with pacman
 pacman_packages=(
     stow
     xdg-desktop-portal-gtk
     thunar
     alacritty
     zsh
-    power-profiles-daemon
+    power-profiles-daemon-openrc
     waybar
     gnome-disk-utility
     brightnessctl
@@ -19,6 +18,7 @@ pacman_packages=(
     network-manager-applet
     blueman
     bluez-utils
+    bluez-openrc
     eza
     bluez
     qt6-svg 
@@ -43,7 +43,6 @@ pacman_packages=(
     starship
 )
 
-# Define packages to be installed with yay
 yay_packages=(
     wlogout
     waybar-module-pacman-updates-git
@@ -55,51 +54,38 @@ yay_packages=(
     hyprshot-git
 )
 
-# Install packages with pacman
-for pkg in "${pacman_packages[@]}"; do
-    echo "Installing $pkg with pacman..."
-    sudo pacman -S --noconfirm $pkg
-    sleep 2 # Delay for readability and control
-done
+echo "Installing all pacman packages..."
+sudo pacman -S --noconfirm "${pacman_packages[@]}"
 
-# Install packages with yay
-for pkg in "${yay_packages[@]}"; do
-    echo "Installing $pkg with yay..."
-    yay -S --noconfirm $pkg
-    sleep 2 # Delay for readability and control
-done
+echo "Installing all yay packages..."
+yay -S --noconfirm --removemake "${yay_packages[@]}"
 
 echo "All packages have been installed."
 
-
-# Make .local if it doesnt exist so stow doesnt symlink the entire .local folder
 mkdir -p ~/.local
-mkdir ~/.local/share
+mkdir -p ~/.local/share
 
-# Run stow to sync dotfiles
 echo "Syncing dotfiles with stow..."
 stow .
 
-# Start daemons
 echo "Starting services..."
-sudo systemctl enable power-profiles-daemon.service
-sudo systemctl start power-profiles-daemon.service
-sudo systemctl enable bluetooth
-sudo systemctl start bluetooth
-sudo systemctl enable sddm
-sudo systemctl set-default graphical.target
-
-# Uninstall useless packages
-sudo pacman -Rsn kitty dunst dolphin vim wofi
+# Add and start power-profiles-daemon
+sudo rc-update add power-profiles-daemon default
+sudo rc-service power-profiles-daemon start
+# Add and start bluetooth
+sudo rc-update add bluetooth default
+sudo rc-service bluetooth start
+# Add and start SDDM
+sudo rc-update add sddm default
 
 # Update the user directories
 echo "Updating user directories..."
 xdg-user-dirs-update
 
 mkdir -p ~/.config/gtk-4.0
-ln -s  ~/.local/share/themes/catppuccin-mocha-blue-standard+default/gtk-4.0/assets ~/.config/gtk-4.0
-ln -s ~/.local/share/themes/catppuccin-mocha-blue-standard+default/gtk-4.0/gtk.css ~/.config/gtk-4.0/gtk.css
-ln -s ~/.local/share/themes/catppuccin-mocha-blue-standard+default/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/gtk-dark.css
+#ln -s  ~/.local/share/themes/catppuccin-mocha-blue-standard+default/gtk-4.0/assets ~/.config/gtk-4.0
+#ln -s ~/.local/share/themes/catppuccin-mocha-blue-standard+default/gtk-4.0/gtk.css ~/.config/gtk-4.0/gtk.css
+#ln -s ~/.local/share/themes/catppuccin-mocha-blue-standard+default/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/gtk-dark.css
 
 sudo flatpak override --filesystem=~/.themes
 sudo flatpak override --filesystem=xdg-config/gtk-4.0
@@ -108,14 +94,11 @@ sudo flatpak override --filesystem=xdg-config/Kvantum:ro
 sudo flatpak override --env=QT_STYLE_OVERRIDE=kvantum
 flatpak install org.kde.KStyle.Kvantum/x86_64/5.15-22.08 org.kde.KStyle.Kvantum/x86_64/5.15-23.08 org.kde.KStyle.Kvantum/x86_64/6.5 org.kde.KStyle.Kvantum/x86_64/6.6 org.kde.KStyle.Kvantum/x86_64/5.15 org.kde.KStyle.Kvantum/x86_64/5.15-21.08
 
-# Change default shell to zsh
 echo "Changing shell to ZSH..."
 chsh -s /bin/zsh
 
-# Make SDDM conf 
 sudo touch /etc/sddm.conf 
 
-# Create a backup of the SDDM configuration file
 echo "Creating a backup of the SDDM configuration file..."
 sudo cp /etc/sddm.conf /etc/sddm.conf.bak
 
@@ -123,13 +106,3 @@ sudo cp /etc/sddm.conf /etc/sddm.conf.bak
 echo "Setting up sddm theme..."
 sudo cp -r  ~/dotfiles/sddm/catppuccin-mocha/ /usr/share/sddm/themes/
 sudo sed -i.bak '/\[Theme\]/,/^\[/s/.*//g' /etc/sddm.conf && echo -e "[Theme]\nCurrent=catppuccin-macchiato" | sudo tee -a /etc/sddm.conf
-
-# Ask the user if they would like to reboot
-read -p "Would you like to reboot now (RECCOMENDED: YES)? (yes/no): " answer
-if [ "$answer" == "yes" ]; then
-    echo "Rebooting..."
-    sudo reboot
-else
-    echo "Skipping reboot."
-fi
-
